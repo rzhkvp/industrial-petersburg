9be037f5cd9c8b4bdd15c5ea457c4cc21d3ba114
Sasha123Alex <103099514+Sasha123Alex@users.noreply.github.com>
2025-05-28 19:05:58 +0300

    Update run_index.py

--- run_index.py
+++ run_index.py
@@ -2,16 +2,16 @@ import webbrowser
 import os
 from pathlib import Path
 
-# Определяем путь к index.html относительно местоположения этого скрипта
+
 script_dir = Path(__file__).parent
 index_path = script_dir / 'index.html'
 
-# Проверяем, существует ли файл index.html
+
 if not index_path.exists():
-    print(f"Файл {index_path} не найден. Убедитесь, что он находится в той же папке, что и скрипт.")
+    print(f"Файл {index_path} не найден.")
 else:
-    # Преобразуем путь к URI, чтобы браузер мог его открыть
+    
     index_uri = index_path.resolve().as_uri()
 
-    # Открываем в браузере по умолчанию
+    
     webbrowser.open_new_tab(index_uri)
